37931c00987f23e0d205a463b6aceefca8e08d10
Alex Stephen <astephen2@gmail.com>
2022-12-07 21:55:49 -0800

    remove bloc, I don't need it yet

--- lib/accounts/bloc/account_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'account_bloc.dart';
-
-abstract class AccountEvent extends Equatable {
-  @override
-  List<Object> get props => [];
-}
-
-class AccountFetched extends AccountEvent {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,10 @@
 import 'package:auth/accounts/view/account_view.dart';
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  Bloc.observer = SimpleBlocObserver();
   runApp(const AuthApp());
 }
 
 class AuthApp extends MaterialApp {
   const AuthApp({super.key}) : super(home: const AccountsPage());
-}
-
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onTransition(
-    Bloc<dynamic, dynamic> bloc,
-    Transition<dynamic, dynamic> transition,
-  ) {
-    super.onTransition(bloc, transition);
-    print(transition);
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    print(error);
-    super.onError(bloc, error, stackTrace);
-  }
 }
