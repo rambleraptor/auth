61ead28467f209be498baf6c968c4ae41d40edd6
Alex Stephen <astephen2@gmail.com>
2022-12-08 20:33:06 -0800

    refactor models

--- lib/accounts/models/accounts.dart
+++ lib/accounts/models/accounts.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:otp/otp.dart';
 
 class Account extends Equatable {
@@ -21,16 +20,6 @@ class Account extends Equatable {
 
   String timeRemaining() {
     return OTP.remainingSeconds().toString();
-  }
-}
-
-class AccountProvider extends ChangeNotifier {
-  final Account account;
-  AccountProvider(this.account);
-
-  void updateCode() {
-    account.code();
-    notifyListeners();
   }
 }
 
