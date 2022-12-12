9120c7fb48ceb23854f3b36916f347397e1bd218
Alex Stephen <astephen2@gmail.com>
2022-12-12 12:16:35 -0800

    first test

--- test/timer_test.dart
+++ test/timer_test.dart
@@ -19,5 +19,20 @@ void main() {
 
     final timeTextFinder = find.text('30');
     expect(timeTextFinder, findsOneWidget);
+
+    final textWidget = timeTextFinder.evaluate().single.widget as Text;
+    expect(textWidget.style!.color, Colors.green);
+  });
+
+  testWidgets('TimeRemainingWidget shows time remaining in red',
+      (tester) async {
+    const testKey = Key('K');
+    await tester.pumpWidget(createWidget(testKey, 5));
+
+    final timeTextFinder = find.text('5');
+    expect(timeTextFinder, findsOneWidget);
+
+    final textWidget = timeTextFinder.evaluate().single.widget as Text;
+    expect(textWidget.style!.color, Colors.red);
   });
 }
