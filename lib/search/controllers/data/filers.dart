2c780207f426542c6276854326c712f9b7548331
Alex Stephen <astephen2@gmail.com>
2022-12-28 21:34:17 -0800

    data model + tests

--- lib/search/controllers/data/filers.dart
+++ lib/search/controllers/data/filers.dart
@@ -1,5 +1,31 @@
+import 'package:auth/accounts/models/accounts.dart';
+
 enum FilterType {
   website,
   username,
   mostCommon,
+}
+
+List<SavedAccount> filterAccounts(
+    List<SavedAccount> accounts, FilterType filterType) {
+  switch (filterType) {
+    case FilterType.username:
+      {
+        accounts.sort((a, b) => a.username.compareTo(b.username));
+      }
+      break;
+
+    case FilterType.website:
+      {
+        accounts.sort((a, b) => a.issuer.compareTo(b.issuer));
+      }
+      break;
+
+    case FilterType.mostCommon:
+      {
+        accounts.sort((a, b) => a.tapped.compareTo(b.tapped));
+      }
+      break;
+  }
+  return accounts;
 }
